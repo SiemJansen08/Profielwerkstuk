6b3ba96fa5484d1e1a9c67613a4787f8abb694f5
JobNijveld <jobnijveld@gmail.com>
2024-09-02 23:08:42 +0200

    bugfixes enemy

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -1,12 +1,16 @@
 extends CharacterBody2D
 
-@export var speed = 30
+@export var speed = 30 #hoger is langzamer
 var player_chase = false
 var player = null
 
 func _physics_process(delta):
 	if player_chase: 
-		position += (player.position - position) / speed
+		velocity = (player.get_global_position() - position).normalized() * speed * delta
+	else:
+		velocity = lerp(velocity, Vector2.ZERO, 0.05)
+	move_and_collide(velocity)
+	move_and_slide()
 		
 
 
