e36d42e0b9fc0fdf7cb3b2ae367e450ae7434c00
JobNijveld <jobnijveld@gmail.com>
2024-09-05 22:01:08 +0200

    npc randomizer

---
+++ scripts/npc_1.gd
@@ -0,0 +1,77 @@
+extends CharacterBody2D
+
+var speed = 30
+var current_state =IDLE
+var is_roaming = true
+var is_chatting = false
+var player
+var player_in_chatzone = false
+var dir = Vector2.RIGHT
+var start_pos
+
+
+enum {
+	IDLE,
+	NEW_DIR,
+	MOVE
+}
+
+func ready():
+	randomize()
+	start_pos = position
+	
+func _process(delta):
+	if current_state == 0 or current_state == 1:
+		$AnimatedSprite2D.play("idle")
+	elif current_state == 2 and !is_chatting:
+		if dir.x == -1:
+			$AnimatedSprite2D.flip_h = true
+			$AnimatedSprite2D.play("side_walk")
+		if dir.x == 1:
+			$AnimatedSprite2D.flip_h = false
+			$AnimatedSprite2D.play("side_walk")
+		if dir.y == -1:
+			$AnimatedSprite2D.flip_h = true
+			$AnimatedSprite2D.play("back_walk")
+		if dir.y == -1:
+			$AnimatedSprite2D.flip_h = true
+			$AnimatedSprite2D.play("front_walk")
+	if is_roaming:
+		match current_state:
+			IDLE:
+				pass
+			NEW_DIR:
+				dir = choose([Vector2.RIGHT, Vector2.LEFT, Vector2.UP, Vector2.DOWN ])
+			MOVE: 
+				move()
+	if Input.is_action_just_pressed("chat"):
+		print("chatting with npc")
+		is_roaming = false
+		is_chatting = true
+		$AnimatedSprite2D.play("idle")
+		
+
+func choose(array):
+	array.shuffle()
+	return array.front()
+	
+func move():
+	if !is_chatting:
+		velocity = dir * speed
+		move_and_slide()
+
+
+func _on_chat_detection_area_body_entered(body):
+	if body.has_method("player"):
+		player = body
+		player_in_chatzone = true
+
+
+func _on_chat_detection_area_body_exited(body):
+	if body.has_method("player"):
+		player_in_chatzone = false
+
+
+func _on_timer_timeout():
+	$Timer.wait_time = choose([0.5, 1, 1.5])
+	current_state = choose([IDLE, NEW_DIR, MOVE])
