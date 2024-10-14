34ba9b43b2fd14cf8f854cb952f551d61b4574a3
JobNijveld <jobnijveld@gmail.com>
2024-10-15 00:12:16 +0200

    painting scripts

---
+++ scripts/paintings/painting6.gd
@@ -0,0 +1,21 @@
+extends Sprite2D
+
+var pickup = false
+var ready_play = false
+
+func _process(delta):
+	if Input.is_action_just_pressed("secure") and pickup == true:
+		Global.paintings = Global.paintings + 1
+		self.queue_free()
+		Global.questlevel = 11
+
+func _on_area_2d_body_entered(body):
+	if body.has_method("player") and ready_play and Global.questlevel >= 11:
+		pickup = true
+		Global.questlevel = 11.1
+
+
+
+
+func _on_start_timer_timeout():
+	ready_play = true
