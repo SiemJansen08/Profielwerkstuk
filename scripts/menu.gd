2bf846608c9cad1019434f7b70114c4ffd070e9f
JobNijveld <jobnijveld@gmail.com>
2024-11-19 23:16:49 +0100

    death glitch bugfix

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -3,15 +3,13 @@ extends Control
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file(Global.current_scene)
-	Global.menu = false
-	print("Deaths: ", Global.deaths)
-	print("Health: ", Global.player_health)
-	if Global.game_first_load == true:
-		Global.questlevel = 1
-	elif Global.player_health == 0:
-		Global.player_health = Global.player_health + 100
-		Global.deaths = Global.deaths + 1
+	if Global.player_health < 20:
+		Global.player_health = 100
+		get_tree().change_scene_to_file(Global.current_scene)
+		Global.menu = false
+	else:
+		get_tree().change_scene_to_file(Global.current_scene)
+		Global.menu = false
 	
 func _on_options_pressed():
 	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
