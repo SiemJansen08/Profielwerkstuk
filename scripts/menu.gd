cea9d380c6da8930da71a14a2238783ce3a94b85
JobNijveld <jobnijveld@gmail.com>
2024-11-21 23:32:24 +0100

    click

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -3,6 +3,8 @@ extends Control
 
 
 func _on_play_pressed():
+	if Global.sound == true:
+		$select.play()
 	if Global.player_health < 20:
 		Global.player_health = 100
 		Global.paintings = 0
@@ -13,12 +15,17 @@ func _on_play_pressed():
 		Global.menu = false
 	
 func _on_options_pressed():
+	if Global.sound == true:
+		$select.play()
 	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
-
+	
 
 func _on_story_pressed():
-	pass # Replace with function body.
+	if Global.sound == true:
+		$select.play()
 
 
 func _on_quit_pressed():
+	if Global.sound == true:
+		$select.play()
 	get_tree().quit()
--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -3,14 +3,20 @@ extends Control
 
 
 func _on_back_pressed():
+	if Global.sound == true:
+		$select.play()
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
 	
 
 
 func _on_difficulty_toggled(toggled_on):
+	if Global.sound == true:
+		$select.play()
 	Global.hard_difficulty = true
 
 func _on_music_pressed():
+	if Global.sound == true:
+		$select.play()
 	if Global.music == true:
 		Global.music = false
 	elif Global.music == false:
@@ -18,6 +24,8 @@ func _on_music_pressed():
 
 
 func _on_sound_pressed():
+	if Global.sound == true:
+		$select.play()
 	if Global.sound == true:
 		Global.sound = false
 	elif Global.sound == false:
