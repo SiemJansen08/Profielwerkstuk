0b44a9919ab6154f18d24a522a02485493c44f84
SiemJansen08 <siemjansen08@gmail.com>
2024-11-23 10:53:17 +0100

    big boss

---
+++ scripts/eindbaas.gd
@@ -0,0 +1,135 @@
+extends CharacterBody2D
+
+signal chase
+
+@export var speed = 15 #hoger is langzamer
+var player_chase = false
+var player = null
+var nochase = false
+
+var health = 99
+var player_inattack_range = false
+var can_take_damage = true
+
+@onready var healthbar = $CanvasLayer/Healthbar
+
+var randomizer = RandomNumberGenerator.new()
+var knockback_state = false
+var knockback_direction = 1
+var knockback_speed = 0.5
+
+var grootte = 2
+
+func _ready():
+	healthbar.init_health(health)
+
+func _physics_process(delta):
+	deal_with_damage()
+	if knockback_state == false:
+		if player_chase:
+			velocity = (player.get_global_position() - position).normalized() * speed * delta
+			
+			if(player.position.x - position.x) < 0:
+				if grootte == 2:
+					$AnimatedSprite2D.play("l_l")
+				elif grootte == 1:
+					$AnimatedSprite2D.play("m_l")
+				elif grootte == 0:
+					$AnimatedSprite2D.play("s_l")	
+			else:
+				if grootte == 2:
+					$AnimatedSprite2D.play("l_r")
+				elif grootte == 1:
+					$AnimatedSprite2D.play("m_r")
+				elif grootte == 0:
+					$AnimatedSprite2D.play("s_r")	
+		
+		else:
+			velocity = lerp(velocity, Vector2.ZERO, 0.05)
+			$AnimatedSprite2D.play("l_l")
+	move_and_collide(velocity)
+	if knockback_state == true:
+		if knockback_direction == 1:
+			velocity.x = -knockback_speed
+			velocity.y = knockback_speed
+		elif knockback_direction == 2:
+			velocity.x = -knockback_speed
+			velocity.y = -knockback_speed
+		elif knockback_direction == 3:
+			velocity.x = knockback_speed
+			velocity.y = knockback_speed
+		elif knockback_direction == 4:
+			velocity.x = knockback_speed
+			velocity.y = -knockback_speed
+	move_and_slide()
+
+	
+func _on_detection_area_body_entered(body): 
+	if Global.current_scene == "res://scenes/cave.tscn":
+		player = body
+		player_chase = true
+	else:
+		player = body
+		nochase = false
+		emit_signal("chase")
+
+func _on_detection_area_body_exited(body):
+		player_chase = false
+		nochase = true
+		
+func enemy():
+	pass
+
+
+func _on_enemy_hitbox_body_entered(body):
+	if body.has_method("player"):
+		player_inattack_range = true
+
+
+func _on_enemy_hitbox_body_exited(body):
+	if body.has_method("player"):
+		player_inattack_range = false
+		
+func deal_with_damage():
+	if player_inattack_range and Global.player_current_attack == true:
+		if can_take_damage == true:
+			health = health - 30
+			healthbar.health = health
+			$knockback.start()
+			knockback_state = true
+			$AnimatedSprite2D.play("damage")
+			$take_damage_cooldown.start()
+			can_take_damage = false
+			print("slime", health)
+			if health <= 0:
+				grootte = grootte - 1
+				health = 99
+				healthbar.queue_redraw()
+				healthbar.health = health
+				if grootte < 0:
+					self.queue_free()
+		
+
+
+
+func _on_take_damage_cooldown_timeout():
+	can_take_damage = true
+
+
+func _on_knockback_timeout():
+	knockback_direction = randomizer.randi_range(0,3)
+	knockback_state = false
+	$knockback.stop()
+
+
+func _on_chase():
+	if nochase == false:
+		player_chase = true
+		if Global.stealth_mode == true:
+			player_chase = false
+		$stealth_check.start()
+
+
+
+func _on_stealth_check_timeout():
+	emit_signal("chase")
---
+++ scripts/healthbar_eindbaas.gd
@@ -0,0 +1,28 @@
+extends ProgressBar
+
+@onready var timer = $Damage_Timer
+@onready var damage_bar = $Damagebar
+
+var health = 0 : set = _set_health
+
+func _set_health(new_health):
+	var prev_health = health
+	health = min(max_value, new_health)
+	value = health
+	
+	
+	if health < prev_health:
+		timer.start()
+	else:
+		damage_bar.value = health
+
+func init_health(_health):
+	health = _health
+	max_value = health
+	value = health
+	damage_bar.max_value = health
+	damage_bar.value = health
+
+
+func _on_damage_timer_timeout():
+	damage_bar.value = health
